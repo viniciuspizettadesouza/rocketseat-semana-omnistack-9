a66345fc063820b334ebb239112e6c61e3183bdc
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-10-03 08:52:49 -0300

    add mongoose

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const routes = require('./routes')
 
 const app = express()
 
-app.get('/', (req, res) => {
-    return res.json({ message: 'Hello World' })
-})
+// GET, POST, PUT, DELETE
+// req.query = acessar query params (para filtros)
+// req.params = acessar route params (para edição, delete)
+// req.body = acessar corpo da requisição (para criação, edição)
+
+// Sequelize para usar banco de dados SQL
+
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333)
