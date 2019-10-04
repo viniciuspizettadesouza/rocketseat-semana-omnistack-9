7347b7b1672af5382bd47020badbb954aa6c90d0
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-10-04 09:20:10 -0300

    mongodb connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const routes = require('./routes')
 
 const app = express()
+
+mongoose.connect('mongodb+srv://zetta:zetta@clusterzetta-mcozz.mongodb.net/omnistack09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 // GET, POST, PUT, DELETE
 // req.query = acessar query params (para filtros)
