fcc520cad65936255df7ea84b8e1f7a2a0c5da08
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-10-06 13:07:11 -0300

    import api, configure submit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './services/api'
 import './App.css';
 
 import logo from './assets/logo.svg';
 
 function App() {
+  const [email, setEmail] = useState('')
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+
+    const response = await api.post('/sessions', {
+      email
+    })
+
+  }
+  function handleEmailChange(event) {
+    setEmail(event.target.value)
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="aircnc" />
       <div className="content">
         <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa</p>
-        <form>
-          <label htmlFor="email">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">E-MAIL
           </label>
           <input
             id="email"
             type="html"
-            placeholder="seu melhor e-mail"
+            placeholder="Seu melhor e-mail"
+            value={email}
+            onChange={handleEmailChange}
           />
 
           <button className="btn" type="submit">Entrar</button>
