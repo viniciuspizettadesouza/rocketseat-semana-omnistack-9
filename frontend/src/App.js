112759ed03bc4b55ffead7483d1c4f0ebc611cda
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-10-06 07:34:09 -0300

    front login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
 
+import logo from './assets/logo.svg';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello Zetta</h1>
-      </header>
+    <div className="container">
+      <img src={logo} alt="aircnc" />
+      <div className="content">
+        <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa</p>
+        <form>
+          <label htmlFor="email">
+          </label>
+          <input
+            id="email"
+            type="html"
+            placeholder="seu melhor e-mail"
+          />
+
+          <button className="btn" type="submit">Entrar</button>
+        </form>
+      </div>
     </div>
   );
 }
