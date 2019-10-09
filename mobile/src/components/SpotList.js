86fe34d1f54c4f3214b3d98c6f18c73331dd6505
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-10-09 01:34:22 -0300

    new func handlenavigate, add reactnavigation

--- mobile/src/components/SpotList.js
+++ mobile/src/components/SpotList.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
+import { withNavigation } from 'react-navigation'
 import { View, Text, StyleSheet, FlatList, Image, TouchableOpacity } from 'react-native'
 
 import api from '../services/api'
 
-export default function SpotList({ tech }) {
+function SpotList({ tech, navigation }) {
     const [spots, setSpots] = useState([])
 
     useEffect(() => {
@@ -16,9 +17,13 @@ export default function SpotList({ tech }) {
         loadSpots()
     }, [])
 
+    function handleNavigate(id) {
+        navigation.navigate('Book', { id })
+    }
+
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Empresas que usam <Text style={bold}>{tech}</Text></Text>
+            <Text style={styles.title}>Empresas que usam <Text style={styles.bold}>{tech}</Text></Text>
             <FlatList
                 style={styles.list}
                 data={spots}
@@ -30,7 +35,7 @@ export default function SpotList({ tech }) {
                         <Image style={styles.thumbnail} source={{ uri: item.thumbnail_url }} />
                         <Text style={styles.company} >{item.company}</Text>
                         <Text style={styles.price} >{item.price ? `R$${item.price}/dia` : GRATUITO}</Text>
-                        <TouchableOpacity onPress{() => { }} style={styles.button} >
+                        <TouchableOpacity onPress={() => handleNavigate(item._id)} style={styles.button} >
                             <Text style={styles.buttonText}  >Solicitar Reserva</Text>
                         </TouchableOpacity>
                     </View>
@@ -47,21 +52,21 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 20,
         color: '#444',
-        paddigHorizontal: 20,
+        paddingHorizontal: 20,
         marginBottom: 15,
     },
     bold: {
         fontWeight: 'bold',
     },
     list: {
-        paddigHorizontal: 20,
+        paddingHorizontal: 20,
     },
     listItem: {
         marginRight: 15,
     },
     thumbnail: {
         width: 200,
-        heigth: 120,
+        height: 120,
         resizeMode: 'cover',
         borderRadius: 2,
     },
@@ -76,7 +81,6 @@ const styles = StyleSheet.create({
         color: '#999',
         marginTop: 5,
     },
-
     button: {
         height: 32,
         backgroundColor: '#f05a5b',
@@ -91,3 +95,5 @@ const styles = StyleSheet.create({
         fontSize: 15,
     },
 })
+
+export default withNavigation(SpotList)
