b1ebca9e70107f93c4a0ccaab86e9b9d5fffa876
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-10-09 01:33:27 -0300

    add scrollview

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { SafeAreaView, StyleSheet, Image, AsyncStorage } from 'react-native'
+import { SafeAreaView, ScrollView, StyleSheet, Image, AsyncStorage } from 'react-native'
 
 import SpotList from '../components/SpotList'
 
@@ -20,9 +20,9 @@ export default function List() {
     return (
         <SafeAreaView style={StyleSheet.container} >
             <Image styles={styles.logo} source={logo} />
-            {
-                techs.map(tech => <SpotList key={tech} tech={tech} />)
-            }
+            <ScrollView>
+                {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+            </ScrollView>
         </SafeAreaView>
     )
 }
